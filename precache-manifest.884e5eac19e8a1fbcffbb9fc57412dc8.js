e0a93ebc07bbc3191606c1fcff0a67b26d0e3e72
Ervandra Halim <ervandra.halim@gmail.com>
2020-03-08 11:33:41 +0700

    Updates

---
+++ precache-manifest.884e5eac19e8a1fbcffbb9fc57412dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79935b2bbd1b4aa523cf736f1b80f03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd6b000c765228bc16e5",
+    "url": "/static/css/main.b5a97c0d.chunk.css"
+  },
+  {
+    "revision": "574a208b0e3d54738e79",
+    "url": "/static/js/2.6d6547e9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.6d6547e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd6b000c765228bc16e5",
+    "url": "/static/js/main.461fad77.chunk.js"
+  },
+  {
+    "revision": "b924f9d69dc57ff0696d",
+    "url": "/static/js/runtime-main.0a9ded9a.js"
+  }
+]);
